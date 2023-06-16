e03927c5326394e1873131525fc59b7a59c1c84a
originalkyu <originalkyu@gmail.com>
2023-06-16 09:28:23 +0900

    06.16

--- practice/algorithm/Permutation.cpp
+++ practice/algorithm/Permutation.cpp
@@ -2,8 +2,23 @@
 #include <vector>
 #include <algorithm> // next_permutation(), prev_permutation()
 #include <utility> // swap
-
 using namespace std;
+
+struct loc {
+    int x,y,z;
+
+    bool operator < (const loc &a) const {
+        if(x==a.x && y==a.y) return z < a.z;
+        if(y==a.y) return y < a.y;
+        return x < a.x;
+    }
+
+    void print_loc() {
+        cout << x << " " << y << " " << z << " ";
+        cout << "\n";
+    }
+};
+
 
 void printV(vector<int> &v) {
     for(int i=0; i<v.size(); i++) {
@@ -46,11 +61,12 @@ void makePermutation(vector<int> &v, int n, int end_depth, int depth) {
 }
 
 int main() {
+    int cnt=0;
     int a[3] = {1,2,3};
     vector<int> v;
 
 
-    // next_permutation()
+    // next_permutation() : 반드시 sort를 먼저 할 것!!!
     cout << "next_permutation()" << "\n";
     for(int i=0; i<3; i++) v.push_back(a[i]);
     do {
@@ -60,12 +76,53 @@ int main() {
     cout << "prev_permutation()" << "\n";
     v.clear();
     for(int i=2; i<=0; i--) v.push_back(a[i]);
+    // do {
+    //     printV(v);
+    // } while(prev_permutation(v.begin(), v.end()));
+
+
+    /*
+        next_permutation and struct
+    */
+    vector<loc> loc_vec1 = {{1,2,6},{1,2,3},{2,3,4},{3,4,5}};
+    cout << "loc_vec------" << "\n";
+    cnt=0;
     do {
-        printV(v);
-    } while(prev_permutation(v.begin(), v.end()));
+        cout << ++cnt << "\n";
+        for(auto el : loc_vec1) el.print_loc();
+    } while(next_permutation(loc_vec1.begin(), loc_vec1.end()));
+
+    // after sort
+    vector<loc> loc_vec2 = {{1,2,6},{1,2,3},{2,3,4},{3,4,5}};
+    sort(loc_vec2.begin(), loc_vec2.end());
+    cout << "loc_vec------" << "\n";
+    cnt=0;
+    do {
+        cout << ++cnt << "\n";
+        for(auto el : loc_vec2) el.print_loc();
+    } while(next_permutation(loc_vec2.begin(), loc_vec2.end()));
+
+    /*
+        next_permutation and char vector
+    */
+    vector<char> ch_vec1 = {'*','+'};
+    vector<char> ch_vec2 = {'+', '*'};
+    cnt=0;
+    do {
+        cout << ++cnt << "\n";
+        for(auto el : ch_vec1) cout << el << " ";
+        cout << "\n";
+    } while(next_permutation(ch_vec1.begin(), ch_vec1.end()));
+
+    cnt=0;
+    do {
+        cout << ++cnt << "\n";
+        for(auto el : ch_vec2) cout << el << " ";
+        cout << "\n";
+    } while(next_permutation(ch_vec2.begin(), ch_vec2.end()));
     // recursive
     cout << "recursive: " << "\n";
     v.clear();
     for(int i=0; i<4; i++) v.push_back(a[i]);
-    makePermutation(v,4,4,0);
+    // makePermutation(v,4,4,0);
 }
--- week5/U15685.cpp
+++ week5/U15685.cpp
@@ -2,26 +2,54 @@
 https://www.acmicpc.net/problem/15685
 */
 #include <iostream>
+#include <cstring>
+#include <vector>
 using namespace std;
 
 int n, x, y, d, g;
 int a[104][104];
+int cnt=0;
+vector<int> v;
+
+const int dy[] = {0,-1,0,1}; // right up left down
+const int dx[] = {1,0,-1,0};
 
 
-struct DD {
-    int x,y,d,g;
-};
+void drawDD(int x, int y, int d, int g, int now_depth) {
+    if(g==now_depth) {
+        int ny = y+dy[v[0]]; int nx = x+dx[v[0]];
+        int nny,nnx;
+        a[ny][nx]=1;
+        for(int i=1; i<v.size(); i++) {
+            nny = ny+dy[v[i]]; nnx = nx+dx[v[i]];
+            a[nny][nnx] = 1;
+            ny = nny; nx = nnx;
+        }
+        return;
+    }
+    for(int i=v.size()-1; i>=0; i--) v.push_back((v[i]+1)%4);
+    drawDD(x,y,d,g,now_depth+1);
+}
 
-
-void drawDD(int x, int y, int d, int g) {
-    
+void checksquare() {
+    for(int i=0; i<100; i++) {
+        for(int j=0; j<100; j++) {
+            if(a[i][j] && a[i+1][j] && a[i][j+1] && a[i+1][j+1]) cnt++;
+        }
+    }
 }
 
 int main() {
+    memset(a, 0, sizeof(a));
     cin >> n;
     for(int i=0; i<n; i++) {
         cin >> x >> y >> d >> g;
-        v.push_back(x,y,d,g);
+        a[y][x] = 1;
+        v.clear();
+        v.push_back(d);
+        drawDD(x,y,d,g,0);
     }
 
+    checksquare();
+    cout << cnt << "\n";
 }
